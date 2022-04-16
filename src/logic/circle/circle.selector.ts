60cdfc02ab8fe66ea5e1b32fc2ba03d2a10b0615
Wojciech Cendrzak <23259795+WojciechCendrzak@users.noreply.github.com>
2022-04-16 18:53:01 +0200

    clear up

--- src/logic/circle/circle.selector.ts
+++
@@ -1,6 +0,0 @@
-import { StoreState } from '../../app/app.reducers';
-
-export const whoWaitForZoneSelector = (zoneKey: string | undefined) => (state: StoreState) =>
-  Object.entries(state.circle.whoWaits)
-    .filter(([key]) => key === zoneKey)
-    .map(([_, value]) => value);
