70ae11b0e7154fe6285a30532f5325ba494dfb83
Wojciech Cendrzak <23259795+WojciechCendrzak@users.noreply.github.com>
2022-04-16 18:52:33 +0200

    clearup

--- src/logic/circle/circle.epic.ts
+++ src/logic/circle/circle.epic.ts
@@ -1,11 +1,10 @@
 import { combineEpics } from 'redux-observable';
-import { from, of } from 'rxjs';
+import { of } from 'rxjs';
 import { filter, map, pluck, switchMap } from 'rxjs/operators';
 import { RootEpic } from '../../app/app.epics.type';
 import { MAX_SPEED } from '../hub/hub.model';
 import { hubSlice } from '../hub/hub.slice';
 import { getZoneKey, zoneControl } from './circle.logic';
-import { whoWaitForZoneSelector } from './circle.selector';
 import { circleSlice } from './circle.slice';
 
 const colorDetected: RootEpic = (actions$, state$) =>
@@ -22,56 +21,6 @@ const colorDetected: RootEpic = (actions$, state$) =>
         ...res.toRun.map((hubId) => hubSlice.actions.changeSpeedTo({ hubId, to: MAX_SPEED }))
       );
     })
-    //   canEnter(state$.value.circle.whoBlocks, hubId, zoneKey)
-    //     ? circleSlice.actions.enterZone({ hubId, zoneKey })
-    //     : circleSlice.actions.waitForZone({ hubId, zoneKey })
-    // )
   );
 
-// const enterZone: RootEpic = (actions$, state$) =>
-//   actions$.pipe(
-//     filter(circleSlice.actions.enterZone.match),
-//     pluck('payload'),
-//     map((payload) => ({ ...payload, currentZone: state$.value.circle[payload.hubId].currentZone })),
-//     switchMap(({ hubId, zoneKey, currentZone }) =>
-//       of(
-//         ...(currentZone
-//           ? [
-//               circleSlice.actions.unblockZone({
-//                 hubId,
-//                 zoneKey: currentZone,
-//               }),
-//             ]
-//           : []),
-//         circleSlice.actions.blockZone({ hubId, zoneKey }),
-//         hubSlice.actions.changeSpeedTo({ hubId, to: MAX_SPEED })
-//       )
-//     )
-//   );
-
-// const unblockZone: RootEpic = (actions$, state$) =>
-//   actions$.pipe(
-//     filter(circleSlice.actions.unblockZone.match),
-//     pluck('payload'),
-//     switchMap(({ zoneKey }) =>
-//       from(
-//         whoWaitForZoneSelector(zoneKey)(state$.value).map((hubId) =>
-//           circleSlice.actions.enterZone({ hubId, zoneKey })
-//         )
-//       )
-//     )
-//   );
-
-// const waitForZone: RootEpic = (actions$) =>
-//   actions$.pipe(
-//     filter(circleSlice.actions.waitForZone.match),
-//     pluck('payload'),
-//     map(({ hubId }) => hubSlice.actions.changeSpeedTo({ hubId, to: 0 }))
-//   );
-
-export const circleEpics = combineEpics(
-  colorDetected
-  // enterZone,
-  //  unblockZone,
-  //  waitForZone
-);
+export const circleEpics = combineEpics(colorDetected);
--- src/logic/circle/circle.slice.ts
+++ src/logic/circle/circle.slice.ts
@@ -2,20 +2,9 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { ColorChain } from '../hub/hub.model';
 import { HubKey, ZoneKey } from './circle.logic';
 
-// export type Semaphores = Record<string, string>;
-
-// const hub = {
-//   currentZone: undefined as string | undefined,
-//   waitForZone: undefined as string | undefined,
-// };
-
 const initialState = {
   whoBloks: {} as Record<ZoneKey, HubKey>,
   whoWaits: {} as Record<ZoneKey, HubKey>,
-  // whoBlocks: {} as Semaphores,
-  // whoWaits: {} as Semaphores,
-  // [HubUUID.One.toString()]: hub,
-  // [HubUUID.Two.toString()]: hub,
 };
 
 export type CircleState = typeof initialState;
@@ -37,26 +26,5 @@ export const circleSlice = createSlice({
       state.whoBloks = whoBloks;
       state.whoWaits = whoWaits;
     },
-    // enterZone: (_state, _action: PayloadAction<{ hubId: string; zoneKey: string }>) => undefined,
-    // waitForZone: (state, action: PayloadAction<{ hubId: string; zoneKey: string }>) => {
-    //   const { hubId, zoneKey } = action.payload;
-    //   state.whoWaits[zoneKey] = hubId;
-    //   state[hubId].waitForZone = zoneKey;
-    // },
-    // unWaitForZone: (state, action: PayloadAction<{ hubId: string; zoneKey: string }>) => {
-    //   const { zoneKey, hubId } = action.payload;
-    //   delete state.whoWaits[zoneKey];
-    //   state[hubId].waitForZone = undefined;
-    // },
-    // blockZone: (state, action: PayloadAction<{ hubId: string; zoneKey: string }>) => {
-    //   const { hubId, zoneKey } = action.payload;
-    //   state.whoBlocks[zoneKey] = hubId;
-    //   state[hubId].currentZone = zoneKey;
-    // },
-    // unblockZone: (state, action: PayloadAction<{ hubId: string; zoneKey: string }>) => {
-    //   const { zoneKey, hubId } = action.payload;
-    //   delete state.whoBlocks[zoneKey];
-    //   state[hubId].currentZone = undefined;
-    // },
   },
 });
